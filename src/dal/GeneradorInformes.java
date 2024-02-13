36c64b9a6b66c92871944ff5374a6b1fb2acfa5c
kasimxo <kasimxo@gmail.com>
2024-02-13 14:04:41 +0100

    Now you can export reports as pdf

--- src/dal/GeneradorInformes.java
+++ src/dal/GeneradorInformes.java
@@ -5,9 +5,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ArrayList;
 
+import java.io.*;
+
 import dataClasses.Articulo;
 import dataClasses.LineaPedido;
 import main.Main;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.Anchor;
+import com.lowagie.text.pdf.PdfWriter;
+import com.lowagie.text.Chunk;
 
 
 /**
@@ -40,8 +49,9 @@ public class GeneradorInformes {
 			}
 		}
 		
-		String titulo = String.format("%-15s %-15s\n", "Artículo", "Cantidad");
-		System.out.print(titulo);
+		String titulo = "Artículo Cantidad";
+		String cabecera = String.format("%-15s %-15s\n", "Artículo", "Cantidad");
+		System.out.printf(cabecera);
 		List<String> texto = new ArrayList<String>();
 		procesado.forEach((K,V) -> {
 			String linea = String.format("%-15s %-15s\n", K, V.toString());
@@ -49,7 +59,7 @@ public class GeneradorInformes {
 			texto.add(linea);
 		});
 		
-		guardarPdf(titulo, texto);
+		guardarPdf(titulo, cabecera, texto);
 		
 	}
 	
@@ -59,7 +69,34 @@ public class GeneradorInformes {
 	 * @param titulo
 	 * @param texto
 	 */
-	public static void guardarPdf(String titulo, List<String> texto) {
+	public static void guardarPdf(String titulo, String cabecera, List<String> texto) {
+		
+		//WINDOWS
+		//File f = new File(".\\files\\archivosEntrada\\");
+		
+		//LINUX
+		File f = new File("./files/informes/"+titulo.replace(' ', '_')+".pdf");
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		Document document = new Document();
+		
+		try {
+
+			PdfWriter pdf = PdfWriter.getInstance(document, new FileOutputStream(f.getAbsolutePath()));
+			
+			document.open();
+	        // step 4: we add a paragraph to the document
+	        document.add(new Paragraph(cabecera)); // Añadimos el párrafo
+	        for (String linea : texto) {
+	        	document.add(new Phrase(linea)); //Añadimos las líneas de texto
+	        }
+	      
+	        document.close(); // Cerramos el documento
+	        System.out.println("Se exportado el informe con éxito.");
+		} catch (Exception e) {
+			System.out.println("Ha surgido un error durante la exportación del informe.");
+		}
 		
 	}
 	
